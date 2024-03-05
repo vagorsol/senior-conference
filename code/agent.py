7297745ac3ed3482f19f08565980ccf407f512ee
Audrey Yang <audrey.butter0930@gmail.com>
2024-03-05 13:38:25 -0500

    implemented basic pathfinding (in general)

--- code/agent.py
+++ code/agent.py
@@ -18,12 +18,21 @@ class Agent(Entity):
         self.grid = grid
         self.finder = AStarFinder(diagonal_movement = DiagonalMovement.always)
 
-    def move(self, dt):
+    def move(self, target, dt):
         start = self.grid.node(int(self.pos.x // TILE_SIZE), int(self.pos.y // TILE_SIZE))
-        end = self.grid.node(26, 31) # pygame.math.Vector2     
+        end = self.grid.node(int(target.x), int(target.y)) 
         path,_ = self.finder.find_path(start, end, self.grid)
         self.grid.cleanup()
-        
+
+        # may be off by decimal points but is fine (probably)
+        if(len(path) > 1):
+            self.direction = (
+                pygame.math.Vector2(path[1].x, path[1].y) - pygame.math.Vector2(start.x, start.y))
+            if (self.direction != 0):
+                self.direction = self.direction.normalize()
+        else:
+            self.direction = pygame.math.Vector2(0, 0)
+
         # setting the walking animation (need to tune this)
         if self.direction.y >= 1:
             self.status = 'down'
@@ -40,6 +49,7 @@ class Agent(Entity):
         self.update_timers()
         self.get_target_pos()
 
-        self.move(dt)
+        target = pygame.math.Vector2(26, 31)
+        self.move(target, dt)
         self.animate(dt)
     
