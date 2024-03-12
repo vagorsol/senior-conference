b0b90b891b5ad252c2c4eda85e814d797385a6b2
Audrey Yang <audrey.butter0930@gmail.com>
2024-03-12 14:20:48 -0700

    pathing now goes roughly along center of tile
    potential issue with hitbox not reaching intended target because the agent ended up in an offset place, but if this issue occurs *then* we'll address it

--- code/agent.py
+++ code/agent.py
@@ -1,10 +1,10 @@
-import player
-from sprites import Generic
 import pygame
-import sys
+import math
+from player import Player
 from entity import *
 from settings import *
 from support import *
+from sprites import Generic
 from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
 from pathfinding.core.diagonal_movement import DiagonalMovement
@@ -33,48 +33,33 @@ class Agent(Entity):
             point_rect.x = x
             point_rect.y = y
             Generic((x, y), point_surf, self.all_sprites)   
-
-    def nearest_coor(self, lst):
-        # TODO: point in a list from current position that has the shortest path
-        # given a list of points, return the point that a) can be reached from tphe current location and 
-        # b) has the shortest path. then return the point i guess??
-        # (probably should return the path too so i don't have to recalculate it... time efficiency woo!!)
-        start = self.grid.node(int(self.pos.x // TILE_SIZE), int(self.pos.y // TILE_SIZE))
-        return_point = start
-        min_len = sys.maxsize # set to some large number. tbd
-        for coor in lst: 
-            # print(coor)
-            end = self.grid.node(int(coor.x), int(coor.y)) 
-            path, path_status = self.finder.find_path(start, end, self.grid)
-            self.grid.cleanup()
-            if (path_status and len(path) < min_len):
-                return_point = coor
-        return return_point
-
-    def choose_target(self):
-        point = self.nearest_coor(self.tree_layer)
-        return point
     
-    def move(self, target, dt):
+    def check_target_intersect(self, target):
+        ceil_pos = (math.ceil(self.pos.x / TILE_SIZE),
+                    math.ceil(self.pos.y / TILE_SIZE))
+        floor_pos = (int(self.pos.x // TILE_SIZE), int(self.pos.y // TILE_SIZE))
+        
+        if(ceil_pos != target 
+           and floor_pos != target 
+           and (ceil_pos[0], floor_pos[1]) != target 
+           and (floor_pos[0], ceil_pos[1]) != target
+        ):
+            return False
+        else:
+            return True
+
+    def move(self, target, dt):        
         start = self.grid.node(int(self.pos.x // TILE_SIZE), int(self.pos.y // TILE_SIZE))
         end = self.grid.node(int(target.x), int(target.y)) 
         path, path_status = self.finder.find_path(start, end, self.grid)
         self.grid.cleanup()
 
-        # because int, cannot do in between grids
-        # most likely wil need to revisit this later
-        # potential pathing issues: because the grid-based-ness, can get caught
-        # in between objects not accounted for in the grid. also can possibly not reach some areas
-        # so have to run a "can i get there" check first
-        # TODO: grid pathing funkiness check
-        # TODO: reset agent position at the beginning of the new day
-        # print(path)
-        if (path_status):
+        if (path_status and path):
             path.pop(0)
-            if(path):
-                # how to make it so it is navigating along "center" of the block?
-                # how to "correct" for this?
-                self.direction = (pygame.math.Vector2(path[0].x, path[0].y) - pygame.math.Vector2(start.x, start.y)).normalize()
+            
+            if(path and not self.check_target_intersect((end.x, end.y))):
+                self.direction = (pygame.math.Vector2((path[0].x + 0.5)  * TILE_SIZE, (path[0].y + 0.5) * TILE_SIZE) 
+                                  - pygame.math.Vector2(self.pos.x, self.pos.y)).normalize()
             else:
                 self.direction = pygame.math.Vector2(0, 0)
                 # flag for when arrived
@@ -84,26 +69,27 @@ class Agent(Entity):
             self.direction = pygame.math.Vector2(0, 0)
         
         # setting the walking animation
-        if self.direction.y >= 1:
+        if self.direction.y > 0:
             self.status = 'down'
-        elif self.direction.y <= -1:
-            self.status = 'up'
-        if self.direction.x >= 1:
+        elif self.direction.y < 0:
+            self.status = 'up' 
+        if self.direction.x > 0 and self.direction.x > self.direction.y:
             self.status = 'right'
-        elif self.direction.x <= -1:
-            self.status = 'left'
-        # self.draw_path(path)
+        elif self.direction.x < 0 and self.direction.x < self.direction.y:
+            self.status = 'left' 
         super().move(dt) 
 
     def update(self, dt):
         self.get_status()
         self.update_timers()
         self.get_target_pos()
-       
-        #15, 7
-        target = pygame.math.Vector2(15, 7)
+        
+        # 15, 7
+        target = pygame.math.Vector2(37, 23)
+        # stops at ~ 36, 23
         self.move(target, dt)
         self.animate(dt)
+        # for behavior make list and cycle through
     
     def reset(self):
         self.pos = pygame.math.Vector2(self.RESET_POS.x, self.RESET_POS.y)
