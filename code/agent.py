10a5351e4bfba2bf2f5b2baa1d53ae9f4cf95150
Audrey Yang <audrey.butter0930@gmail.com>
2024-03-16 00:03:17 -0700

    behaviors implementation start

--- code/agent.py
+++ code/agent.py
@@ -1,6 +1,8 @@
 import pygame
 import math
 from player import Player
+from status import Status
+from behavior import WaterBehavior, SeedBehavior, TreeBehavior
 from entity import *
 from settings import *
 from support import *
@@ -18,10 +20,28 @@ class Agent(Entity):
         self.tree_layer = tree_layer
         self.grid = grid
         self.finder = AStarFinder(diagonal_movement = DiagonalMovement.never) 
+        self.RESET_POS = pygame.math.Vector2(pos[0], pos[1])
+        self.movement = Status.NOT_RUNNING
+        self.target = None
+
+        # pathfinding debugging values
         self.screen = screen
         self.all_sprites = all_sprites
-        self.RESET_POS = pygame.math.Vector2(pos[0], pos[1])
 
+        # set up the behavior tree
+        self.setup()
+
+    def setup(self):
+        water_behavior = WaterBehavior(self, self.soil_layer.unwatered_tiles, self.grid)
+        seed_behavior = SeedBehavior(self, self.soil_layer.empty_soil_tiles, self.grid)
+        tree_behavior = TreeBehavior(self, self.tree_layer, self.grid)
+
+        water_behavior.set_next_behavior(seed_behavior)
+        seed_behavior.set_next_behavior(tree_behavior)
+
+        self.curr_behavior = tree_behavior
+
+    # pathing debugging function
     def draw_path(self, path):
         for point in path:
             x = point.x * TILE_SIZE
@@ -38,35 +58,41 @@ class Agent(Entity):
         ceil_pos = (math.ceil(self.pos.x / TILE_SIZE),
                     math.ceil(self.pos.y / TILE_SIZE))
         floor_pos = (int(self.pos.x // TILE_SIZE), int(self.pos.y // TILE_SIZE))
-        
-        if(ceil_pos != target 
-           and floor_pos != target 
-           and (ceil_pos[0], floor_pos[1]) != target 
-           and (floor_pos[0], ceil_pos[1]) != target
+        # print("checking")
+        # print(ceil_pos)
+        # print(floor_pos)
+        # print(ceil_pos[0], floor_pos[1])
+        # print(floor_pos[0], ceil_pos[1])
+        # print(target)
+        if(ceil_pos == target 
+           or floor_pos == target 
+           or (ceil_pos[0], floor_pos[1]) == target 
+           or (floor_pos[0], ceil_pos[1]) == target
         ):
-            return False
-        else:
             return True
+        else:
+            return False
 
-    def move(self, target, dt):        
+    def move(self, dt):        
         start = self.grid.node(int(self.pos.x // TILE_SIZE), int(self.pos.y // TILE_SIZE))
-        end = self.grid.node(int(target.x), int(target.y)) 
+        end = self.grid.node(int(self.target.x), int(self.target.y)) 
         path, path_status = self.finder.find_path(start, end, self.grid)
         self.grid.cleanup()
-
+        # print(start)
         if (path_status and path):
             path.pop(0)
-            
+            # print(path)
+            # print(self.check_target_intersect((end.x, end.y)))
             if(path and not self.check_target_intersect((end.x, end.y))):
                 self.direction = (pygame.math.Vector2((path[0].x + 0.5)  * TILE_SIZE, (path[0].y + 0.5) * TILE_SIZE) 
                                   - pygame.math.Vector2(self.pos.x, self.pos.y)).normalize()
+                self.movement = Status.RUNNING # flag for "currently moving"
             else:
                 self.direction = pygame.math.Vector2(0, 0)
-                # flag for when arrived
-                # enum - success / fail
-                # this code should go into a different function since the move is the actually moving it function
+                self.movement = Status.SUCCESS # flag for arrival
         else: 
             self.direction = pygame.math.Vector2(0, 0)
+            self.movement = Status.FAILURE # flag for failure
         
         # setting the walking animation
         if self.direction.y > 0:
@@ -77,6 +103,7 @@ class Agent(Entity):
             self.status = 'right'
         elif self.direction.x < 0 and self.direction.x < self.direction.y:
             self.status = 'left' 
+        # self.draw_path(path)
         super().move(dt) 
 
     def update(self, dt):
@@ -84,10 +111,11 @@ class Agent(Entity):
         self.update_timers()
         self.get_target_pos()
         
-        # 15, 7
-        target = pygame.math.Vector2(37, 23)
-        # stops at ~ 36, 23
-        self.move(target, dt)
+        # target = pygame.math.Vector2(37, 23)
+        if (self.curr_behavior != None):
+            self.curr_behavior.update()
+        if (self.movement is not Status.SUCCESS):
+            self.move(dt)
         self.animate(dt)
         # for behavior make list and cycle through
     
--- code/behavior.py
+++ code/behavior.py
@@ -1,4 +1,5 @@
 import sys
+from status import Status
 from timer import Timer
 from settings import *
 from pathfinding.core.grid import Grid
@@ -6,64 +7,85 @@ from pathfinding.finder.a_star import AStarFinder
 from pathfinding.core.diagonal_movement import DiagonalMovement 
 
 class Behavior():
-    def _init_(self, agent, list, grid):
+    def __init__(self, agent, list, grid):
         self.agent = agent
         self.list = list
         self.grid = grid
         self.finder = AStarFinder(diagonal_movement = DiagonalMovement.never)
 
-        # setting as none because need to make the other 
-        # behavior BEFORE linking them
+        # setting as none because need to make the other behavior BEFORE linking them
+        self.status = Status.NOT_RUNNING
         self.next_behavior = None 
         # TODO: status and ticks()
-    
-    def nearest_path(self, pos, lst):
+
+    def set_path(self, pos, lst):
         start = self.grid.node(int(pos.x // TILE_SIZE), int(pos.y // TILE_SIZE))
         min_len = sys.maxsize
         path_status = False
         nearest_coor = start
-        shortest_path = []
+
         for coor in lst: 
             end = self.grid.node(int(coor.x), int(coor.y)) 
             path, path_status = self.finder.find_path(start, end, self.grid)
             self.grid.cleanup()
             if (path_status and len(path) < min_len):
-                nearest_coor = end
-                shortest_path = path    
-        return nearest_coor, shortest_path, path_status
-    
-    def select_path(self):
-        coor,_ ,_ = self.nearest_path(self.agent.pos, self.list) 
-        return coor
+                nearest_coor = end 
+        self.agent.target = nearest_coor  
     
     def set_next_behavior(self, behavior):
         self.next_behavior = behavior
 
-class WaterBehavior(Behavior):
-    def _init_(self, agent, list, grid):
-        super()._init_(agent, list, grid) 
-        
-   # todo: rename these
     def action(self):
-        # set the agent's selected tool to watering can 
+        pass
+    # TODO: have the actions be loopable (don't progress to the next one until a "failure" happens)
+
+    def update(self):
+        # if the agent is not moving, set a target
+        if (self.agent.movement == Status.NOT_RUNNING and self.status == Status.NOT_RUNNING):
+            self.set_path(self.agent.pos, self.list)
+        else:
+            if (self.agent.movement == Status.SUCCESS):
+                self.agent.movement = Status.NOT_RUNNING
+                self.status = Status.RUNNING
+                self.action()
+            elif (self.agent.movement == Status.FAILURE):
+                # reset statuses
+                self.agent.movement = Status.NOT_RUNNING
+                self.status = Status.NOT_RUNNING
+
+                # set current behavior to this's next behavior
+                self.agent.curr_behavior = self.next_behavior
+        # also TODO gotta have this action loop until can't do anything, i.e. list is empty. wahoo
+
+class WaterBehavior(Behavior):
+    def __init__(self, agent, list, grid):
+        super().__init__(agent, list, grid) 
+        
+    def action(self):
+        # set the agent's selected tool to watering can and use it
         self.agent.tool_index = 2 
         self.agent.timers['tool use'].activate()
 
 class SeedBehavior(Behavior):
-    def _init_(self, agent, list, grid):
-        super()._init_(agent, list, grid)   
+    def __init__(self, agent, list, grid):
+        super().__init__(agent, list, grid)   
+    
+    def action(self):
+        self.agent.timers['seed use'].activate()
+        # need to pick seed and check that there are seeds to be planted first too
 
 class HarvestBehavior(Behavior):
-    def _init_(self, agent, list, grid):
-        super()._init_(agent, list, grid)   
+    def __init__(self, agent, list, grid):
+        super().__init__(agent, list, grid)   
         # need "list of fully grown plant" tiles
         # or maybe it's just "if picked something up, place it in bin"
 
 class TreeBehavior(Behavior):
-    def _init_(self, agent, tree_layer, grid):
-        super()._init_(agent, tree_layer, grid)  
+    def __init__(self, agent, list, grid):
+        super().__init__(agent, list, grid)  
         
     def action(self):
-        # set the agent's selected tool to axe
+        # set the agent's selected tool to axe and then swing
+        # TODO: for tree, need some way of knowing that it is done (i.e., tree is "dead")
         self.agent.selected_tool = 1 
         self.agent.timers['tool use'].activate()
